b9acdd46140b719a0798c71e0a36f6ee8a23872a
zhf8 <1715005995@qq.com>
2023-07-31 14:45:36 +0800

    拆分前后端仓库，删除老版本

--- v2.0/web/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: './' //解决二级页面白屏问题
-})
